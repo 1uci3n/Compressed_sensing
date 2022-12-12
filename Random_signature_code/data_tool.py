361e36fb8ed72b57096365412081a05a85f4ea05
1uci3n <giranten@outlook.com>
2022-12-12 17:00:11 +0900

    fix some comments and name of functions

--- Random_signature_code/data_tool.py
+++ Random_signature_code/data_tool.py
@@ -2,7 +2,7 @@
 # @Author: weilantian
 # @Date:   2021-01-04 22:44:06
 # @Last Modified by:   1uci3n
-# @Last Modified time: 2022-12-09 14:59:08
+# @Last Modified time: 2022-12-12 16:57:35
 
 # @Python_version: 3.9.1
 
@@ -17,11 +17,11 @@ def get_random_binary_signature_matrix_1_inverse1(user_number, code_length):
     (0.5,0.5)
 
     Args:
-        user_number: the total number of user
-        code_length: the code length of signature code
+        user_number: the total number of user.
+        code_length: the code length of signature code.
 
     Returns:
-        matrix signature matrix
+        matrix signature matrix.
     '''
     matrix = 1 - (2 * np.random.binomial(1,0.5, (user_number,code_length)))
     return matrix
@@ -32,11 +32,11 @@ def get_random_triad_signature_matrix_0_1_inverse1(user_number, code_length):
     (0.5,0.25,0.25)
 
     Args:
-        user_number: the total number of user
-        code_length: the code length of signature code
+        user_number: the total number of user.
+        code_length: the code length of signature code.
 
     Returns:
-        matrix signature matrix
+        matrix signature matrix.
     '''
     matrix = 1 - (2 * np.random.binomial(1,0.5, (user_number,code_length)))
     return matrix * np.random.binomial(1,0.5, (user_number,code_length))
@@ -47,40 +47,61 @@ def get_random_binary_signature_matrix_0_1(user_number, code_length):
     (0.5,0.5)
 
     Args:
-        user_number: the total number of user
-        code_length: the code length of signature code
+        user_number: the total number of user.
+        code_length: the code length of signature code.
 
     Returns:
-        matrix signature matrix
+        matrix signature matrix.
     '''
     matrix = np.random.binomial(1,0.5, (user_number,code_length))
     return matrix
 
 def get_random_complex_gauss_matrix(m, n, loc=0, scale=0.5):
     '''
-    m numbr of rows
-    n number of columns
-    loc Mean (“centre”) of the distribution
-    scale Standard deviation (spread or “width”) of the distribution
+    a function to get complex Gaussian matrix by give the shape
+
+    Args:
+        m: numbr of the rows.
+        n: number of the columns.
+        loc Mean (“centre”) of the distribution
+        scale Standard deviation (spread or “width”) of the distribution
+
+    Returns:
+        matrix a complex Gaussian matrix.
     '''
     original_gauss_matrix = np.random.normal(loc, scale, (m, n, 2))
-    return original_gauss_matrix.view(np.complex128).reshape(m, n)
+    matrix original_gauss_matrix.view(np.complex128).reshape(m, n)
+    return matrix
 
 def get_random_complex_1_inv_1_matrix(m, n):
     '''
-    m numbr of rows
-    n number of columns
+    a function to get complex (1,-1)-matrix by give the shape
+
+    Args:
+        m: numbr of the rows.
+        n: number of the columns.
+
+    Returns:
+        matrix a complex (1,-1)-matrix.
     '''
     original_1_inv_1_matrix = (np.random.binomial(1,0.5,(10,10,2)).astype(np.float64)) * 2 - 1
-    return original_1_inv_1_matrix.view(np.complex128)
+    matrix original_1_inv_1_matrix.view(np.complex128)
+    return matrix
 
 def get_random_complex_1_0_matrix(m, n):
     '''
-    m numbr of rows
-    n number of columns
+    a function to get complex (1, 0)-matrix by give the shape
+
+    Args:
+        m: numbr of the rows.
+        n: number of the columns.
+
+    Returns:
+        matrix a complex (1, 0)-matrix.
     '''
     original_1_0_matrix = np.random.binomial(1,0.5,(10,10,2)).astype(np.float64)
-    return original_1_0_matrix.view(np.complex128)
+    matrix = original_1_0_matrix.view(np.complex128)
+    return matrix
 
 def get_gauss_random_signature_matrix(user_number, code_length, mean=0, std=1 ):
     return np.random.normal(mean, std, size=(user_number, code_length))
@@ -120,19 +141,44 @@ def get_dataset(size, signature_matrix, acteve_rate=0.1, is_fading=False, fading
     Y_set = np.dot(H_set, signature_matrix)
     return Y_set, X_set, H_set
 
-def get_dataset_with_noise(size, signature_matrix, snr_dB, acteve_rate=0.1, is_fading=False, fading_scale=1.0, fading_mode=1, print_var=False, classic_noise=False):
+def get_dataset_with_double_side_noise(size, signature_matrix, snr_dB, acteve_rate=0.1, is_fading=False, fading_scale=1.0, fading_mode=1, print_var=False, classic_noise=False):
+    '''
+    a function to generate the random data set by give set size and signature matrix 
+    with doubel side noise
+
+    Args:
+        size: The size of data set.
+        signature_matrix: The sensing matrix is used to
+            generated the received signal.
+        snr_dB: dB value of snr
+        acteve_rate The user's active probability,
+            the default value is 0.1.
+        is_fading: To control whether the generated data contains fading,
+            the default value is 0 (false)
+        fading_scale: To set the fading scale,
+            the default value is 1.
+        fading_mode: To set the fading mode,
+            the default value is 1 (Rayleigh fading).
+        print_var: print the variance of the noise distribution，
+            the default value is False.
+        classic_noise: should use the classic formula to calculate the power
+            power(signal,2) or not abs(signal)
+
+    Returns:
+        Y_set_with_noise received signal with noise, X_set user status vector,
+        H_set fading coefficient vector, Y_set received signal without noise
+    '''
     Y_set, X_set, H_set = get_dataset(size, signature_matrix, acteve_rate=acteve_rate, is_fading=is_fading, fading_scale=fading_scale, fading_mode=fading_mode)
-    # if classic_noise:
-        # single_bit_power = get_average_power_of_signature_matrix_old(signature_matrix, np.shape(signature_matrix)[1], acteve_rate=acteve_rate)
-    # else:
-    single_bit_power = get_average_power_of_signature_matrix(signature_matrix, np.shape(signature_matrix)[1], acteve_rate=acteve_rate)
-    snr_rate = snr_tool_dB_2_rate(snr_dB)
+    if classic_noise:
+        single_bit_power = get_average_single_bit_power_of_signature_matrix_old(signature_matrix, np.shape(signature_matrix)[1], acteve_rate=acteve_rate)
+    else:
+        single_bit_power = get_average_single_bit_power_of_signature_matrix(signature_matrix, np.shape(signature_matrix)[1], acteve_rate=acteve_rate)
+    snr_rate = dB_2_rate(snr_dB)
     noise_variance = (single_bit_power / snr_rate) / 2
     Y_set_with_noise = Y_set + np.random.normal(loc=0, scale=np.sqrt(noise_variance), size=Y_set.shape)
     if print_var:
         print(noise_variance)
     return Y_set_with_noise, X_set, H_set, Y_set
-
 
 def test_power_of_signature_matrix_fixed(signature_matrix, code_length, acteve_rate=0.1, test_data_size=1000):
     if code_length != np.shape(signature_matrix)[1] :
@@ -144,13 +190,13 @@ def test_power_of_signature_matrix_fixed(signature_matrix, code_length, acteve_r
     single_bit_power = entire_signal_power / code_length
     return single_bit_power
 
-def get_average_power_of_signature_matrix(signature_matrix, code_length, acteve_rate=0.1):
+def get_average_single_bit_power_of_signature_matrix(signature_matrix, code_length, acteve_rate=0.1):
     if code_length != np.shape(signature_matrix)[1] :
         print('code length error')
         return None
     return np.abs(signature_matrix).mean() * np.shape(signature_matrix)[0] * acteve_rate
 
-def get_average_power_of_signature_matrix_old(signature_matrix, code_length, acteve_rate=0.1):
+def get_average_single_bit_power_of_signature_matrix_old(signature_matrix, code_length, acteve_rate=0.1):
     if code_length != np.shape(signature_matrix)[1] :
         print('code length error')
         return None
@@ -166,12 +212,6 @@ def test_power_of_signature_matrix_err(signature_matrix, code_length, acteve_rat
     entire_signal_power = sum_power / test_data_size
     single_bit_power = entire_signal_power / code_length
     return single_bit_power
-
-def snr_tool_dB_2_rate(dB):
-    return np.power(10., dB/10.)
-
-def snr_tool_rate_2_dB(rate):
-    return 10. * np.log10(rate)
 
 def dB_2_rate(dB):
     return np.power(10., dB/10.)
